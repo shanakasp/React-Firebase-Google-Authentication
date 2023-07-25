7c878a2ab2957665c20fad5c80dac8212a81119a
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-25 23:12:13 +0530

    changed 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 
+import { signInWithGoogle } from './firebase-config';
+
+
 function App() {
   return (
     <div className="App">
-   
+   <button onClick={signInWithGoogle}>Sign In With Google</button>
     </div>
   );
 }
